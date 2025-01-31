e5f019f3e2f274d833b180bd06c324a2bb1880f6
somaharini <somaharini24@gmail.com>
2025-01-31 08:15:11 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Pattern6 {
+    public static void printPattern(int n) {
+        int i, j;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (i == 2 && (j == 1 || j == 3)) {
+                    System.out.print("0 ");
+                } else if (i == 1 && (j == 2)) {
+                    System.out.print("0 ");
+                } else if (i == 3 && (j == 2)) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printPattern(n);
+    }
+}
